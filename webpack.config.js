1fc298ba73c4fc646432a05b1a3698b9e1efb250
Stéphanie <stef-x06@hotmail.fr>
2019-03-31 20:02:51 +0200

    update browser versions

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,15 @@ var config = {
         test : /\.jsx?/,
         exclude: /node_modules/,
         use: {
-          loader : 'babel-loader'
+          loader : 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env',
+              {
+                targets: {browsers: ["last 2 versions", "ie >= 9"]},
+                debug: true
+              }]],
+            plugins: ['@babel/plugin-syntax-dynamic-import']
+          }
         }
       },
       {
