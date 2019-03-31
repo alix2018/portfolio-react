e659155bed9bd0f8f2f859639af93443b7440133
Stéphanie <stef-x06@hotmail.fr>
2019-03-31 17:47:27 +0200

    setup project + build process

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+var APP_DIR = path.resolve(__dirname, 'src');
+var BUILD_DIR = path.resolve(__dirname, 'dist');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    filename: '[name].bundle.js',
+    path: BUILD_DIR
+  },
+  devtool: 'inline-source-map',
+  module : {
+    rules: [
+      {
+        test : /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader : 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: APP_DIR + '/index.html',
+      filename: 'index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: BUILD_DIR,
+    port: 9000,
+    hot: true
+  },
+};
+
+module.exports = config;
