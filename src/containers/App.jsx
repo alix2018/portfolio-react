dfd8f14937fa8d4110591bb9d893825bf82ef11b
Stéphanie <stef-x06@hotmail.fr>
2019-04-23 21:46:33 +0200

    update first page design

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import './App.css';
 
 class App extends React.Component {
   render () {
-    return <p>Hello React!</p>;
+    return (
+    <React.Fragment>
+      <div className="Shape1"></div>
+      <div className="Container">
+        <div className="Title Animated FadeInUp">I am Stéphanie Alix</div>
+        <div className="Social Animated FadeInUp">
+          <a href="https://www.linkedin.com/in/stephanie-alix/" target="_blank">
+            <div className="Linkedin"></div>
+          </a>
+          <a href="https://join.skype.com/invite/ljzSCwTgKRyX" target="_blank">
+            <div className="Skype"></div>
+          </a>
+          <a href="https://github.com/alix2018" target="_blank">
+            <div className="Github"></div>
+          </a>
+        </div>
+        <div>
+          <button className="Button Animated FadeInUp">Download CV</button>
+        </div>
+      </div>
+    </React.Fragment>
+    );
   }
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
+import './index.css';
 
 // Main app
 import App from './containers/App.jsx';
