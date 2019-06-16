c2ab4a79b8f3abc747fb84cafd1a06cf785e3c32
Stéphanie <stef-x06@hotmail.fr>
2019-06-16 19:16:12 +0200

    implement scrolling menu

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './MenuBar.css';
 
 function MenuBar() {
+  const [menuItems, setMenuItems] = useState([]);
+
+  function handleClick(e, className) {
+    e.preventDefault();
+    const currentAnchor = document.querySelector(className);
+    currentAnchor.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
+
+  useEffect(() => {
+    setMenuItems([
+      {id: 1, name: 'Home', className: '.home'},
+      {id: 2, name: 'Portfolio', className: '.portfolio'},
+      {id: 3, name: 'About Me', className: '.about-me'},
+      {id: 4, name: 'Contact', className: '.contact'}
+    ]);
+  });
+
   return (
     <ul className="menu-effect animated fadeInUp">
-      <li className="current"><a href="#">Home</a></li>
-      <li><a href="#">Portfolio</a></li>
-      <li><a href="#">About me</a></li>
-      <li><a href="#">Contact</a></li>
+      {menuItems.map(({id, name, className}) => (
+        <li key={id}>
+          <a key={id} href={`${name}`} onClick={e => {handleClick(e, className)}}>
+            {name}
+          </a>
+        </li>
+      ))}
     </ul>
   );
 }
