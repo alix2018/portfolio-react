6f641866537dfbb56819350e2569649f3eeb37c0
Stéphanie <stef-x06@hotmail.fr>
2019-07-16 20:02:50 +0200

    start design home page

--- src/components/PageHome/PageHome.js
+++ src/components/PageHome/PageHome.js
@@ -5,27 +5,37 @@ import Typewriter from '../Typewriter/Typewriter';
 function PageHome() {
   return (
     <div id="home" className="home">
-      <div className="shape1"/>
-      <div className="container">
-        <div className="title animated fadeInUp">I am Stéphanie</div>
+      <div className="left">
+        <div className="full-name animated fadeInUp">
+          Stéphanie Alix
+        </div>
+        <div className="iam animated fadeInUp">
+          I am
+        </div>
+        <div className="arrow-1"/>
+        <div className="arrow-2"/>
+        <div className="arrow-3"/>
+      </div>
+      <div className="right">
         <Typewriter
+          className="typewriter animated fadeInUp"
           period={200}
           fullTexts={['Web developer', 'From France', 'Working in Amsterdam']}
         />
-        <div className="social animated fadeInUp">
-          <a href="https://www.linkedin.com/in/stephanie-alix/" target="_blank" rel="noopener noreferrer">
+        <div className="social">
+          <form method="get" action="./public/assets/cv.pdf" className="btn-animation animated fadeInUp" target="_blank" rel="noopener noreferrer">
+            <button type="button" className="button">Download CV</button>
+          </form>
+          <a href="https://www.linkedin.com/in/stephanie-alix/" className="linkedin-animation animated fadeInUp" target="_blank" rel="noopener noreferrer">
             <div className="linkedin"/>
           </a>
-          <a href="https://join.skype.com/invite/ljzSCwTgKRyX" target="_blank" rel="noopener noreferrer">
+          <a href="https://join.skype.com/invite/ljzSCwTgKRyX" className="skype-animation animated fadeInUp" target="_blank" rel="noopener noreferrer">
             <div className="skype"/>
           </a>
-          <a href="https://github.com/alix2018" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/alix2018" className="github-animation animated fadeInUp" target="_blank" rel="noopener noreferrer">
             <div className="github"/>
           </a>
         </div>
-        <form method="get" action="./public/assets/cv.pdf" className="btn-animation animated fadeInUp" target="_blank" rel="noopener noreferrer">
-          <button type="button" className="button">Download CV</button>
-        </form>
       </div>
     </div>
   );
