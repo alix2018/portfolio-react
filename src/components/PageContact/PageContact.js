755ffe941f9d083670a7edd568550c1c021c9af3
Stéphanie <stef-x06@hotmail.fr>
2019-08-03 14:53:29 +0200

    update arrows

--- src/components/PageContact/PageContact.js
+++ src/components/PageContact/PageContact.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './PageContact.css';
 
 function PageContact() {
+  function clickArrowsUp(e) {
+    e.preventDefault();
+    const currentAnchor = document.querySelector('.home');
+    currentAnchor.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
   return (
     <section id="contact" className="contact">
       <div className="left">
-        <div className="arrow-1"/>
-        <div className="arrow-2"/>
-        <div className="arrow-3"/>
+        <div className="group-arrows" onClick={e => {clickArrowsUp(e);}}>
+          <div className="arrow-1"/>
+          <div className="arrow-2"/>
+          <div className="arrow-3"/>
+        </div>
         <div className="rights-reserved">© 2019. All Rights Reserved.</div>
         <div className="name">Stéphanie Alix</div>
       </div>
@@ -21,11 +31,11 @@ function PageContact() {
             <a href="https://www.linkedin.com/in/stephanie-alix/" target="_blank" rel="noopener noreferrer">
               <div className="linkedin"/>
             </a>
-            <a href="https://join.skype.com/invite/ljzSCwTgKRyX" target="_blank" rel="noopener noreferrer">
-              <div className="skype"/>
-            </a>
             <a href="https://github.com/alix2018" target="_blank" rel="noopener noreferrer">
               <div className="github"/>
+            </a>
+            <a href="https://join.skype.com/invite/ljzSCwTgKRyX" target="_blank" rel="noopener noreferrer">
+              <div className="skype"/>
             </a>
           </div>
         </section>
