113ecd50927f1e4d7fe07b3cbc7000989bc31932
Stéphanie <stef-x06@hotmail.fr>
2019-08-03 15:41:36 +0200

    portfolio titles

--- src/components/PagePortfolio/PagePortfolio.js
+++ src/components/PagePortfolio/PagePortfolio.js
@@ -3,7 +3,14 @@ import './PagePortfolio.css';
 
 function PagePortfolio() {
   return (
-    <section id="portfolio" className="portfolio">PORTFOLIO</section>
+    <section id="portfolio" className="portfolio">
+      <div className="titles">
+        <h1>Loyalty Application</h1>
+        <h1>Retailer Portal</h1>
+        <h1>2D Games</h1>
+        <h1>Facebook Chatbot</h1>
+      </div>
+    </section>
   );
 }
 
