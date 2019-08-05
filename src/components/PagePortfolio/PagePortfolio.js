6490a4549f890ec8716eff22b58a264294ee4f14
Stéphanie <stef-x06@hotmail.fr>
2019-08-05 21:16:54 +0200

    start design portfolio

--- src/components/PagePortfolio/PagePortfolio.js
+++ src/components/PagePortfolio/PagePortfolio.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './PagePortfolio.css';
 
 function PagePortfolio() {
+  const projectsList = ['Loyalty Application', 'Retailer Portal', '2D Games', 'Facebook Chatbot'];
+  const projectPictures = ['loyalty-application', 'retailer-portal', '2d-game', 'facebook-chatbot'];
+
   return (
     <section id="portfolio" className="portfolio">
-      <div className="titles">
-        <h1>Loyalty Application</h1>
-        <h1>Retailer Portal</h1>
-        <h1>2D Games</h1>
-        <h1>Facebook Chatbot</h1>
+      <div className="left">
+        <div className="titles">
+          {projectsList.map((projectName, index) => {
+            return (
+              <section key={projectName}>
+                <h1>{projectName}</h1>
+                <div className="project-index">0{index + 1}</div>
+              </section>
+            );
+          })}
+        </div>
+      </div>
+      <div className="right">
+        {projectPictures.map(project => {
+          return <img key={project} src={`../../../public/images/${project}.png`}/>;
+        })}
       </div>
     </section>
   );
