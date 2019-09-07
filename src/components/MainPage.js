591e2cbddaf9e3f1be72f1c549e6d8d5f9c6329e
Stéphanie <stef-x06@hotmail.fr>
2019-09-07 14:41:43 +0200

    working snap point

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,15 +4,27 @@ import PageHome from './PageHome/PageHome';
 import PagePortfolio from './PagePortfolio/PagePortfolio';
 import PageAboutMe from './PageAboutMe/PageAboutMe';
 import PageContact from './PageContact/PageContact';
+import './MainPage.css';
+import '../scroll-snap-polyfill';
 
 function MainPage() {
   return (
     <>
       <MenuBar/>
-      <PageHome/>
-      <PagePortfolio/>
-      <PageAboutMe/>
-      <PageContact/>
+      <div id="vertical-snapping">
+        <div className="page">
+          <PageHome/>
+        </div>
+        <div className="page">
+          <PagePortfolio/>
+        </div>
+        <div className="page">
+          <PageAboutMe/>
+        </div>
+        <div className="page">
+          <PageContact/>
+        </div>
+      </div>
     </>
   );
 }
