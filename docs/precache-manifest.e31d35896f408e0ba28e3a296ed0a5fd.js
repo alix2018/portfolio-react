d8e145f8b03f1384b48c043cc7a41eab6397215f
Stéphanie <stef-x06@hotmail.fr>
2019-12-01 12:22:56 +0100

    push docs folder

---
+++ docs/precache-manifest.e31d35896f408e0ba28e3a296ed0a5fd.js
@@ -0,0 +1,88 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "icon_460x460.673b23b17ce9ae314d5e41b38ba3c8f9.png"
+  },
+  {
+    "revision": "12431487282e8170ad524d253f0d2689",
+    "url": "index.html"
+  },
+  {
+    "revision": "f823e98d00812be7d83b",
+    "url": "main.bundle.js"
+  },
+  {
+    "revision": "548e49774908698bd1c7239ee139d38c",
+    "url": "main.bundle.js.LICENSE"
+  },
+  {
+    "url": "main.c2cfc4157ba63dc95fc6.css"
+  },
+  {
+    "revision": "0ea40a06b7991f3f422008027e2bc688",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6de39d825c3d4fd03e0",
+    "url": "npm.hookrouter.bundle.js"
+  },
+  {
+    "revision": "57bfb24e93fe6f14ba5e",
+    "url": "npm.react-dom.bundle.js"
+  },
+  {
+    "revision": "ae36a73193d705b927439cfb873364f9",
+    "url": "npm.react-dom.bundle.js.LICENSE"
+  },
+  {
+    "revision": "4c3029c4990a82478af236f539729744",
+    "url": "public/assets/arrow.svg"
+  },
+  {
+    "revision": "c25588aae41254a00cee4ad316ce19a4",
+    "url": "public/assets/isometric/facebook-chatbot.png"
+  },
+  {
+    "revision": "5f880501744f2aeb557ff1bace0a08b5",
+    "url": "public/assets/isometric/games.png"
+  },
+  {
+    "revision": "e8e0a620046a5ce5d3b6660247c77c2f",
+    "url": "public/assets/isometric/loyalty-application.png"
+  },
+  {
+    "revision": "867867f89aaedc42faef7443c8c29b9d",
+    "url": "public/assets/isometric/retailer-portal.png"
+  },
+  {
+    "revision": "8a41353d0e3f79b4e2896f8e7d9cdb66",
+    "url": "public/assets/logo.png"
+  },
+  {
+    "revision": "be373c049e23688e675960fc093702cd",
+    "url": "public/assets/play-game.svg"
+  },
+  {
+    "revision": "933f60a3f11ba5ec8b464c5bcd61c358",
+    "url": "public/assets/quotation-mark.svg"
+  },
+  {
+    "revision": "44ab2917d6b275663c2ccfdd716ff8c1",
+    "url": "public/assets/self-picture.jpg"
+  },
+  {
+    "revision": "922cb3bc86ec0568c82525e315387021",
+    "url": "public/fonts/Poppins-Bold.ttf"
+  },
+  {
+    "revision": "c1c9dae06c12275f9ddb8667eae1987d",
+    "url": "public/fonts/Poppins-Extra-Bold.ttf"
+  },
+  {
+    "revision": "fcac2dd0845b381fd34f0d902dd6e87b",
+    "url": "public/fonts/Poppins-Light.ttf"
+  },
+  {
+    "revision": "28bc024aa3164e0b0ff2d709f17b333f",
+    "url": "public/fonts/Poppins-Medium.ttf"
+  }
+]);
