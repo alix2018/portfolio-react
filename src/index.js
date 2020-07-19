0128d8f2c74e4ab5117b38bda4e033d5bb275a56
Stéphanie <stef-x06@hotmail.fr>
2020-07-19 17:00:12 +0200

    fix indentation

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import App from './App';
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
-    .then(reg => {
-      console.log('Service worker registered.', reg);
-    });
+      .then(reg => {
+        console.log('Service worker registered.', reg);
+      });
   });
 }
 
