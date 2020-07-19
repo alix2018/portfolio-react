7cc8faed69781568724dfbf1f23d5bf452bf8e8d
Stéphanie <stef-x06@hotmail.fr>
2020-07-19 16:58:58 +0200

    fix sw

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import App from './App';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(reg => {
+      console.log('Service worker registered.', reg);
+    });
   });
 }
 
