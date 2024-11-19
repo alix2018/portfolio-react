f16d3fa8dd8acd143870d5f7518b640af625bc70
alix2018 <stephanie.alix95@gmail.com>
2024-11-19 15:14:14 +0100

    remove commented code

--- webpack.dev.js
+++ webpack.dev.js
@@ -18,38 +18,4 @@ module.exports = merge(common, {
       "@": path.resolve(__dirname, "src"),
     },
   },
-  // mini-css-extract-plugin doesn't support hrm right now
-  // https://github.com/webpack-contrib/mini-css-extract-plugin/issues/126
-  // plugins: [
-  //   new MiniCssExtractPlugin({
-  //     filename: "[name].css",
-  //     chunkFilename: "[id].css",
-  //   }),
-  // ],
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.module\.css$/,
-  //       use: [
-  //         MiniCssExtractPlugin.loader, // Replaces style-loader
-  //         {
-  //           loader: "css-loader",
-  //           options: {
-  //             importLoaders: 1,
-  //             modules: true,
-  //             localIdentName: "[name]__[local]___[hash:base64:5]", // Unique class names for CSS Modules
-  //           },
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       test: /\.css$/, // Global CSS (not CSS Modules)
-  //       exclude: /\.module\.css$/, // Exclude CSS Modules
-  //       use: [
-  //         MiniCssExtractPlugin.loader, // Replaces style-loader
-  //         "css-loader", // No modules option here
-  //       ],
-  //     },
-  //   ],
-  // },
 });
